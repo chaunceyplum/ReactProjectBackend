fe1641d2b2dcb495facc18678b0fb76db2c36c16
Chauncey <Chaunceyplum@gmail.com>
2021-11-14 21:15:39 -0500

    fixed small error

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api', usersRouter);
+// app.use('/api', usersRouter);
 app.use('/cuts', cutsRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,21 @@
-var express = require('express');
-var router = express.Router();
-const cors = require('./cors')
-const AuthController = require('../comtrollers/AuthController')
-const User =require('../models/User')
+// var express = require('express');
+// var router = express.Router();
+// const cors = require('./cors')
+// const AuthController = require('../comtrollers/AuthController')
+// const User =require('../models/User')
 
-router.post('/register', AuthController.register)
-.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-.post(cors.cors, (req, res, next) => {
-    User.create(req.body)
-    .then(user => {
-      res.statusCode = 200
-      res.setHeader('Content-Type', 'application/vnd.api+json')
-      res.json({user})
-    })
+// router.post('/register', AuthController.register)
+// .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+// .post(cors.cors, (req, res, next) => {
+//     User.create(req.body)
+//     .then(user => {
+//       res.statusCode = 200
+//       res.setHeader('Content-Type', 'application/vnd.api+json')
+//       res.json({user})
+//     })
     
-})
-/* GET users listing. */
+// })
+// /* GET users listing. */
 
 
-module.exports = router;
+// module.exports = router;
